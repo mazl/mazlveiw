48cd78852816d1cf00b2a31367a1cbf479cbb0e0
tywetyty@gmail.com <tywetyty@gmail.com>
2023-02-10 12:39:37 +0800

    排序

--- code.py
+++ code.py
@@ -9,5 +9,12 @@ with open('./res.csv','w') as csvfile:
         for i in data[1:]:
             writer.writerow(list(data[0][1:]+i))
 
-  
+with open('./res.csv', 'r', newline='') as f_input:
+    csv_input = csv.DictReader(f_input)
+    data = sorted(csv_input, key=lambda row: (row['year']))
+
+with open('./output.csv', 'w', newline='') as f_output:    
+    csv_output = csv.DictWriter(f_output, fieldnames=csv_input.fieldnames)
+    csv_output.writeheader()
+    csv_output.writerows(data)
 
